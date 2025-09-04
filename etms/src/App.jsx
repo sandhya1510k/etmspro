65b4d04a1eb02284335302fc74e2f2667acc0c4e
sandhya1510k <sandhyabujji1510@gmail.com>
2025-09-04 10:57:06 +0530

    update

---
+++ etms/src/App.jsx
@@ -0,0 +1,18 @@
+import AdminDashboard from './Pages/Admin/AdminDashboard'
+import Navbar from './Components/Navbar'
+import Sidebar from './Components/Sidebar'
+function App() {
+
+  return (
+    <>
+      <Sidebar />
+      <Navbar />
+      <AdminDashboard />
+
+
+
+    </>
+  )
+}
+
+export default App
